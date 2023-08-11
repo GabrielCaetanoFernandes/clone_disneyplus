ac1289967604208930c766ffd571da2e5c9c126e
Gabriel Caetano <caetano9650@gmail.com>
2023-08-11 16:13:54 -0300

    criação do header e ajustes de responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question');
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const position = window.scrollY;
+
+        if(position < heroHeight){
+            hideHeader();
+        } else{
+            displayHeader();
+        }
+    })
+
+    //attraction tabs
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const tabTarget = botao.target.dataset.tabButton;
@@ -12,7 +25,30 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //FAQ accordion
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', openCloseAnswer)
+    }
 })
+
+function hideHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function displayHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function openCloseAnswer(elemento){
+    const active = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(active)
+
+}
 
 function removeActiveBtn(){
     const buttons = document.querySelectorAll('[data-tab-button]');
